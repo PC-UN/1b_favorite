6a3b476bed858054c9d00560c99d5269bf354af7
Jairo Suárez <jaasuarezga@unal.edu.co>
2018-05-16 10:42:41 -0500

    Change host invalid character

--- b1_favorite_ms/settings.py
+++ b1_favorite_ms/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'favorite_db',
+        'HOST': 'favorite-db',
         'PORT': 5432,
     }
 }
